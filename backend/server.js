e154a3e5b46277d70a9b905bfd14f79c344f55d0
anish <anishdangi.me@gmail.com>
2025-05-20 14:37:24 +0545

    backend-setup

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+//Connect DB
+connectDB();
+
+//Middleware
+app.use(express.json());
+
+//Routes
+// app.use("/api/auth", authRoutes)
+// app.use("/api/users", userRoutes)
+// app.use("/api/tasks", taskRoutes)
+// app.use("/api/reports", reportRoutes)
+
+//Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
